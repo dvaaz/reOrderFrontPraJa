f10415499d372fa7b7d7b1469f95a299ac7f1fb7
ADS 2024.1M <36129382024.1M@senacrjedu.local>
2025-12-08 11:05:29 -0300

    interfaces

--- src/constants/interfaces.ts
+++ src/constants/interfaces.ts
@@ -1,16 +1,56 @@
 export interface Grupo {
   id: number;      // A API disse que é Integer
   nome: string;    // A API disse que é String
-  cor: string;     // A API disse que é String (precisaremos adicionar o #)
-  tipo: number;    // A API disse que é Integer
-  status: number;  // A API disse que é Integer
+  cor: string;     // A API disse que é String (preciso adicionar o #)
+  tipo: number;    
+  status: number;  
 }
 
 export interface Ingrediente {
-  id: number;               // A API disse que é Integer
-  nome: string;            // A API disse que é String
-  unidade_medida: string;  // A API disse que é String
-  grupo?: Grupo;     // A API disse que é um objeto do tipo Grupo ou null
-  status: number;          // A API disse que é Integer
+  id: number;               
+  nome: string;           
+  unidade_medida: string; 
+  grupo?: Grupo;     
+  status: number;         
+}
+
+export interface Ficha {
+  id: number;
+  nome: string;
+  descricao: string;
+  grupo?: Grupo;
+  status?: number;
+}
+
+export interface Estoque {
+  id: number;
+  quantidade: number;
+  entrada: string;
+  validade: string;
+  ingrediente: Ingrediente;
+  status?: number;
+}
+
+export interface IngredienteFicha {
+  id: number;
+  ingrediente: Ingrediente;
+  ficha: Ficha;
+  unidadeMedida: number;
+  quantidade: number;
+  status?: number;
+}
+
+export interface ProducaoDia {
+  id: number;
+  data: string;
+  status?: number;
+}
+
+export interface Producao {
+  id: number;
+  quantidade: number;
+  estoque: Estoque;
+  producaoDia: ProducaoDia;
+  status?: number;
 }
 
